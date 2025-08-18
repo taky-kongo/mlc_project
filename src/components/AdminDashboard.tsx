4c836a12cce73aeb2a73a8b182dccd8ae96973d1
Taky Kongo <abdoulmalickkongo@gmail.com>
2025-08-18 09:37:23 +0000

    Ajout de bcrypt dans le fichier requirements

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -31,7 +31,7 @@ const AdminDashboard: React.FC = () => {
         setError('');
         try {
             //const response = await fetch('http://127.0.0.1:8000/admin', {
-            const response = await fetch('https://mon-back-mlc.onrender.com/api/admin', {
+            const response = await fetch('https://mon-back-mlc.onrender.com/admin', {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
